c2dae4998363f9dcb9b5ac16eb0d75dd7256a47a
Jerry Lin <cylin520@ntu.edu.tw>
2021-12-07 20:42:44 +0800

    1st version

--- Homework014/ViewController.swift
+++ Homework014/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var upButton: UIButton!
+    
+    @IBOutlet weak var upButton2: UIButton!
+    
+    
+    @IBOutlet weak var upButton3: UIButton!
+    
+    
+    @IBOutlet weak var upButton4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        upButton.layer.borderWidth = 1
+        upButton.layer.borderColor = UIColor(named: "buttonGray")?.cgColor
+        upButton.layer.cornerRadius = 15
+        
+        upButton2.layer.borderWidth = 1
+        upButton2.layer.borderColor = UIColor(named: "buttonGray")?.cgColor
+        upButton2.layer.cornerRadius = 15
+        
+        upButton3.layer.borderWidth = 1
+        upButton3.layer.borderColor = UIColor(named: "sunYellow")?.cgColor
+        upButton3.layer.cornerRadius = 15
+        
+        upButton4.layer.borderWidth = 1
+        upButton4.layer.borderColor = UIColor(named: "buttonGray")?.cgColor
+        upButton4.layer.cornerRadius = 15
+        
     }
 
 
