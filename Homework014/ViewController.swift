06498f044dfb5790869e2cc4145cb2d031465b2e
Jerry Lin <cylin520@ntu.edu.tw>
2021-12-08 21:52:48 +0800

    Complete

--- Homework014/ViewController.swift
+++ Homework014/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var upButton: UIButton!
-    
     @IBOutlet weak var upButton2: UIButton!
-    
-    
     @IBOutlet weak var upButton3: UIButton!
-    
-    
     @IBOutlet weak var upButton4: UIButton!
+    @IBOutlet weak var plateForm1: UIView!
+    @IBOutlet weak var plateForm2: UIView!
+    @IBOutlet weak var plateForm3: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +36,22 @@ class ViewController: UIViewController {
         upButton4.layer.borderColor = UIColor(named: "buttonGray")?.cgColor
         upButton4.layer.cornerRadius = 15
         
+        
+        plateForm1.layer.shadowOffset = CGSize(width: 5, height: 5)
+        plateForm1.layer.shadowOpacity = 0.7
+        plateForm1.layer.shadowRadius = 5
+        plateForm1.layer.shadowColor = UIColor(named: "shadow")?.cgColor
+
+        plateForm2.layer.shadowOffset = CGSize(width: 5, height: 5)
+        plateForm2.layer.shadowOpacity = 0.7
+        plateForm2.layer.shadowRadius = 5
+        plateForm2.layer.shadowColor = UIColor(named: "shadow")?.cgColor
+        
+        plateForm3.layer.shadowOffset = CGSize(width: 5, height: 5)
+        plateForm3.layer.shadowOpacity = 0.7
+        plateForm3.layer.shadowRadius = 5
+        plateForm3.layer.shadowColor = UIColor(named: "shadow")?.cgColor
+        plateForm3.backgroundColor = .white
     }
 
 
